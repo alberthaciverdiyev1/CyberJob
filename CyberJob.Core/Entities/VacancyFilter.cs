0be19dab00b3a173432bce0051664568f798ed1f
alberthaciverdiyev1 <alberthaciverdiyev55@gmail.com>
2026-02-04 08:17:35 -0500

    z

--- CyberJob.Core/Entities/VacancyFilter.cs
+++ CyberJob.Core/Entities/VacancyFilter.cs
@@ -6,6 +6,6 @@ public class VacancyFilter : BaseEntity
     public virtual Vacancy? Vacancy { get; set; }
 
     public int FilterId { get; set; }
-    public virtual Filter? Filter { get; set; }
+    public virtual Filter? Filter { get; set; } 
     
 }
