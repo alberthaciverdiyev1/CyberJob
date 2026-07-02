4fe82b7dd2cfa7fe3102b62a2ad04fb477a5a7a7
alberthaciveridyev1 <alberthaciverdiyev55@gmail.com>
2026-07-02 15:08:11 -0700

    refactored theme initialization by moving logic from `theme.js` to `_Layout.cshtml` and adjusted styles and layouts in Rank views

--- CyberJob/wwwroot/js/theme.js
+++ CyberJob/wwwroot/js/theme.js
@@ -1,14 +1,3 @@
-﻿(function () {
-    const userTheme = localStorage.getItem('theme');
-    const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
-
-    if (userTheme === 'dark' || (!userTheme && systemTheme)) {
-        document.documentElement.classList.add('dark');
-    } else {
-        document.documentElement.classList.remove('dark');
-    }
-})();
-
 document.addEventListener('DOMContentLoaded', () => {
     const toggleBtn = document.getElementById('theme-toggle');
     if (toggleBtn) {
