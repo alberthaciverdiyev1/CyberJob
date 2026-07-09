e9c20c1fcacc33a53d313a958788b248803f2e8d
alberthaciveridyev1 <alberthaciverdiyev55@gmail.com>
2026-07-09 12:59:35 -0700

    added social media support for TikTok, YouTube, and Twitter/X with updates to models, Vacancy details view, and footer layout

--- CyberJob/Models/Setting.cs
+++ CyberJob/Models/Setting.cs
@@ -1,4 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CyberJob.Models;
@@ -12,44 +12,53 @@ public class Setting
 
     [Column("phone_number")]
     public string? PhoneNumber { get; set; }
-    
+
     [Column("whatsapp_number")]
     public string? WhatsappNumber { get; set; }
-    
+
     [Column("whatsapp_business_number")]
     public string? WhatsappBusinessNumber { get; set; }
-    
+
     [Column("instagram_url")]
     public string? InstagramUrl { get; set; }
-    
+
     [Column("facebook_url")]
     public string? FacebookUrl { get; set; }
-    
+
     [Column("linkedin_url")]
     public string? LinkedinUrl { get; set; }
-    
+
     [Column("telegram_number")]
     public string? TelegramNumber { get; set; }
-    
+
+    [Column("tiktok_url")]
+    public string? TiktokUrl { get; set; }
+
+    [Column("youtube_url")]
+    public string? YoutubeUrl { get; set; }
+
+    [Column("twitter_url")]
+    public string? TwitterUrl { get; set; }
+
     [Column("mail")]
     public string? Mail { get; set; }
-    
+
     [Column("address")]
     public string? Address { get; set; }
-    
+
     [Column("working_hours")]
     public string? WorkingHours { get; set; }
-    
+
     [Column("header_scripts")]
     public string? HeaderScripts { get; set; }
-    
+
     [Column("body_scripts")]
     public string? BodyScripts { get; set; }
-    
+
     [Column("footer_scripts")]
     public string? FooterScripts { get; set; }
 
-    // Laravel $table->timestamps() karşılıkları
+    // Laravel $table->timestamps() counterparts
     [Column("created_at")]
     public DateTime? CreatedAt { get; set; }
 
